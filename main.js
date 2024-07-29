958069e8206aa6263b49b92a8068243e82b2f968
carloseduuejfj <110061942+carloseduuejfj@users.noreply.github.com>
2024-07-29 14:27:18 +0000

    .

---
+++ main.js
@@ -0,0 +1,15 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "quem descobriu o Brasil?",
+        alternativas: [
+            {
+                texto: "bolsonaro",
+                afirmação: "Não foi o bolsonaro que descobriu o Brasil."
+            },
+            {
+                texto: "pedro alvares cabral",
+                afirmação: "parabens, voce acertou quem descobriu o Brasil."
+            }
+        ]
+    }
+];
